e1ca6210fce2077cebca6cb655a80420691f8efe
Patrik Třeštík <trestik@bakalari.cz>
2020-01-23 22:30:19 +0100

    First API client for tests

---
+++ src/DiasendAPI.Client/DiasendClient.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Net.Http;
+
+namespace DiasendAPI
+{
+    public class DiasendClient
+    {
+        private readonly string username;
+        private readonly string password;
+
+        public DiasendClient(string username, string password)
+        {
+            this.username = username;
+            this.password = password;
+        }
+        public void GetPatientData()
+        {
+            using(var client = new Client(username,password))
+            {
+                client.BaseUrl = "https://api.diasend.com/1/";
+                client.DataAllAsync("", null, null, null);
+            }
+        }
+    }
+}

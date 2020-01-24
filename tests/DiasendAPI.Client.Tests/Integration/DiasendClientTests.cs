380cb7d41433f2d02e9be32ed7f46487e14a0380
PatrikT <PatrikTrestik@users.noreply.github.com>
2020-01-25 00:25:49 +0100

    - Fixed API definition (#4)
    - tests: Reading device list, data

---
+++ tests/DiasendAPI.Client.Tests/Integration/DiasendClientTests.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Extensions.Configuration;
+using DiasendAPI;
+using System.Threading.Tasks;
+using System.Linq;
+
+namespace DiasendAPI.Client.Tests.Integration
+{
+    [TestClass]
+    public class DiasendClientTests
+    {
+        public DiasendClientTests()
+        {
+            var builder = new ConfigurationBuilder()
+                .AddUserSecrets<DiasendClientTests>();
+
+            Configuration = builder.Build();
+        }
+
+        IConfiguration Configuration { get; set; }
+
+        [TestMethod]
+        public async Task ReadPatientDevices()
+        {
+            var username = Configuration["ApiUsername"];
+            var password = Configuration["ApiPassword"];
+            var testSerial = "SM30226252"; //Change this to serial of your your device
+
+            var client = new DiasendClient(username,password);
+
+            var devices = await client.GetRegisteredDevices();
+            Assert.IsNotNull(devices);
+            Assert.IsTrue(devices.Any(d => d.Serial == username)); //There is always listed user among the devices
+            var device = devices.FirstOrDefault(d => d.Serial == testSerial);
+            Assert.IsNotNull(device);
+            var lastData = device.Last_value_at;
+            Assert.IsNotNull(lastData);            
+
+            var data=await client.GetPatientData(lastData.AddDays(-10), lastData);
+            Assert.IsNotNull(data);
+            Assert.IsTrue(data.Any());
+        }
+
+    }
+}

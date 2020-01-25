3057886c772922e17112d4d0e6b2a40cbb441ab7
Patrik Třeštík <trestik@bakalari.cz>
2020-01-25 15:40:39 +0100

    Upload API and Test - test is failing

--- src/DiasendAPI.Client/DiasendClient.cs
+++ src/DiasendAPI.Client/DiasendClient.cs
@@ -1,22 +1,36 @@
 ﻿using System;
+using System.Buffers.Text;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
 using System.Threading.Tasks;
 
 namespace DiasendAPI
-{
+{    
+    /// <summary>
+    /// Diasend API cleint wrapper. High level methods.
+    /// </summary>
     public class DiasendClient
     {
         private readonly string username;
         private readonly string password;
+        private readonly Guid randomUuid = new Guid("0A645BC8-C18F-4A4C-8C5C-24F68B804C89");
+        private readonly Guid glucoseUuid = new Guid("00001808-0000-1000-8000-00805f9b34fb");
 
+        /// <summary>
+        /// Initialize client.
+        /// </summary>
+        /// <param name="username">Diasend servers User id</param>
+        /// <param name="password">Diasend servers User password</param>
         public DiasendClient(string username, string password)
         {
             this.username = username;
             this.password = password;
         }
-
+        /// <summary>
+        /// Reads registered/known 'devices'
+        /// </summary>
+        /// <returns>List of Device</returns>
         public async Task<IEnumerable<Device>> GetRegisteredDevices()
         {
             using (var client = new Client(username, password))
@@ -26,7 +40,12 @@ namespace DiasendAPI
                 return data.Select(d=>d.Device);
             }
         }
-
+        /// <summary>
+        /// Reads all patient data between dates. Number of results is server limited, keep interval small.
+        /// </summary>
+        /// <param name="from"></param>
+        /// <param name="to"></param>
+        /// <returns></returns>
         public async Task<IEnumerable<DataItem>> GetPatientData(DateTimeOffset from, DateTimeOffset to)
         {
             using(var client = new Client(username,password))
@@ -34,6 +53,27 @@ namespace DiasendAPI
                 client.BaseUrl = "https://api.diasend.com/1/";
                 var data= await client.GetPatientDataAsync(Type.Combined, null, from, to);
                 return data;
+            }
+        }
+        /// <summary>
+        /// Uploads reading to server
+        /// </summary>
+        /// <param name="payload">Raw byte array. GATT??</param>
+        /// <returns></returns>
+        public async Task UploadGlucoseData(byte[] payload)
+        {
+            using (var client = new Client(username, password))
+            {
+                client.BaseUrl = "https://api.diasend.com/1/";
+                var data = new DataUpload()
+                {
+                    Device_type = "GATTProfileGlucose",
+                    Pcode = username,
+                    Password = password,
+                    Uuid = glucoseUuid.ToString("n"),
+                    Payload = Convert.ToBase64String(payload)
+                };
+                await client.PostPatientDataAsync(data);
             }
         }
     }
--- tests/DiasendAPI.Client.Tests/Integration/DiasendClientTests.cs
+++ tests/DiasendAPI.Client.Tests/Integration/DiasendClientTests.cs
@@ -22,8 +22,9 @@ namespace DiasendAPI.Client.Tests.Integration
 
         IConfiguration Configuration { get; set; }
 
+        [TestCategory("Integration API test")]
         [TestMethod]
-        public async Task ReadPatientDevices()
+        public async Task ReadData()
         {
             var username = Configuration["ApiUsername"];
             var password = Configuration["ApiPassword"];
@@ -42,6 +43,53 @@ namespace DiasendAPI.Client.Tests.Integration
             var data=await client.GetPatientData(lastData.AddDays(-10), lastData);
             Assert.IsNotNull(data);
             Assert.IsTrue(data.Any());
+        }
+
+        [TestCategory("Integration API test")]
+        [TestMethod]
+        public async Task UploadData()
+        {
+            var username = Configuration["ApiUsername"];
+            var password = Configuration["ApiPassword"];
+            var sequence = BitConverter.GetBytes(0); //Sequence number ?? 2 bytes
+            byte unknown2 = 0x00;
+            var testDate = DateTime.Now;
+            var year = BitConverter.GetBytes(testDate.Year); //Not sure if correct as LittleEndian
+            var month = BitConverter.GetBytes(testDate.Month);
+            var day = BitConverter.GetBytes(testDate.Day);
+            var hour = BitConverter.GetBytes(testDate.Hour);
+            var minute = BitConverter.GetBytes(testDate.Minute);
+            var second = BitConverter.GetBytes(testDate.Second);
+            var bGmolL = Half.GetBytes(new Half(9.5));
+            var testPayload = new byte[] { 
+                0x00, 0x00, // 0- leading 2b
+                0x0B, // 2 -flags, 0x01 - time offset present, 0x02 - glucose data present, 0x04 - mol/l, 0x08 - sensor status present, 0x10 - context info follows
+                sequence[0], sequence[1], // 3 - sequence 2b 
+                year[0], year[1], // 5
+                month[0], // 6
+                day[0], // 7
+                hour[0], // 8
+                minute[0], // 9
+                second[0], // 10
+                0x01, 0x00, // 11 - time offset 2b, presaent if flags0=1
+                bGmolL[0], bGmolL[1], // 13 - sFloat 2b ??, present if flags1=1, unit  flags2= 0 (kg/L), 1 (mol/l)
+                0x00, // 15 - sample location and type ??
+                0xF8, // 16 - status ??
+            };
+
+            var client = new DiasendClient(username, password);
+
+            //Status: 500
+            //Response: { "error":"invalid_request","error_description":"Internal terminal serial failure."}
+            try
+            {
+                await client.UploadGlucoseData(testPayload);
+            }
+            catch(Exception ex)
+            {
+                Assert.Fail(ex.Message);
+            }
+            
         }
 
     }
